159272d8e093bf97552fdd6f0c03da2d806e6c23
Alex <alex.r@gms.info>
2018-07-17 22:41:33 +0300

    bf2

--- data/mgr/002.sql
+++ data/mgr/002.sql
@@ -27,3 +27,9 @@ ALTER TABLE hotel_config ADD COLUMN is_hidden TINYINT(1) DEFAULT '0';
 
 ALTER TABLE booking ADD COLUMN is_backend_viewed TINYINT(1) DEFAULT '0';
 ALTER TABLE booking ADD COLUMN payment_status VARCHAR(255) DEFAULT 'nopayment' AFTER hash;
+
+ALTER TABLE booking ADD COLUMN lang VARCHAR(2) AFTER payment_status;
+
+ALTER TABLE hotel_config ADD COLUMN created_at DATETIME NOT NULL;
+
+ALTER TABLE hotel_config ADD COLUMN  updated_at DATETIME NOT NULL;
--- lib/filter/doctrine/base/BaseHotelConfigFormFilter.class.php
+++ lib/filter/doctrine/base/BaseHotelConfigFormFilter.class.php
@@ -13,23 +13,27 @@ abstract class BaseHotelConfigFormFilter extends BaseFormFilterDoctrine
   public function setup()
   {
     $this->setWidgets(array(
-      'hotel_id'  => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Hotel'), 'add_empty' => true)),
-      'keyname'   => new sfWidgetFormFilterInput(array('with_empty' => false)),
-      'section'   => new sfWidgetFormFilterInput(),
-      'datatype'  => new sfWidgetFormFilterInput(),
-      'label'     => new sfWidgetFormFilterInput(),
-      'value'     => new sfWidgetFormFilterInput(),
-      'is_hidden' => new sfWidgetFormChoice(array('choices' => array('' => 'yes or no', 1 => 'yes', 0 => 'no'))),
+      'hotel_id'   => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Hotel'), 'add_empty' => true)),
+      'keyname'    => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'section'    => new sfWidgetFormFilterInput(),
+      'datatype'   => new sfWidgetFormFilterInput(),
+      'label'      => new sfWidgetFormFilterInput(),
+      'value'      => new sfWidgetFormFilterInput(),
+      'is_hidden'  => new sfWidgetFormChoice(array('choices' => array('' => 'yes or no', 1 => 'yes', 0 => 'no'))),
+      'created_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'updated_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
     ));
 
     $this->setValidators(array(
-      'hotel_id'  => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('Hotel'), 'column' => 'id')),
-      'keyname'   => new sfValidatorPass(array('required' => false)),
-      'section'   => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
-      'datatype'  => new sfValidatorPass(array('required' => false)),
-      'label'     => new sfValidatorPass(array('required' => false)),
-      'value'     => new sfValidatorPass(array('required' => false)),
-      'is_hidden' => new sfValidatorChoice(array('required' => false, 'choices' => array('', 1, 0))),
+      'hotel_id'   => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('Hotel'), 'column' => 'id')),
+      'keyname'    => new sfValidatorPass(array('required' => false)),
+      'section'    => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
+      'datatype'   => new sfValidatorPass(array('required' => false)),
+      'label'      => new sfValidatorPass(array('required' => false)),
+      'value'      => new sfValidatorPass(array('required' => false)),
+      'is_hidden'  => new sfValidatorChoice(array('required' => false, 'choices' => array('', 1, 0))),
+      'created_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'updated_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
     ));
 
     $this->widgetSchema->setNameFormat('hotel_config_filters[%s]');
@@ -50,14 +54,16 @@ abstract class BaseHotelConfigFormFilter extends BaseFormFilterDoctrine
   public function getFields()
   {
     return array(
-      'id'        => 'Number',
-      'hotel_id'  => 'ForeignKey',
-      'keyname'   => 'Text',
-      'section'   => 'Number',
-      'datatype'  => 'Text',
-      'label'     => 'Text',
-      'value'     => 'Text',
-      'is_hidden' => 'Boolean',
+      'id'         => 'Number',
+      'hotel_id'   => 'ForeignKey',
+      'keyname'    => 'Text',
+      'section'    => 'Number',
+      'datatype'   => 'Text',
+      'label'      => 'Text',
+      'value'      => 'Text',
+      'is_hidden'  => 'Boolean',
+      'created_at' => 'Date',
+      'updated_at' => 'Date',
     );
   }
 }
